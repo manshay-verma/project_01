977e80d625d2febd3a5856a49d4057b96487d27f
manshay_verma <manshayverma4@gmail.com>
2025-07-08 11:50:21 +0530

    Use Effect

--- my-app/src/component/MessageCard.js
+++ my-app/src/component/MessageCard.js
@@ -1,10 +1,21 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 function MessageCard({sender, text}){
     const [like, setLikes] = useState(0);
+    const [isRead, setIsRead] = useState(false);
+
     const handleLikes = () => {
         setLikes(like+1);//update state
-    }
+    } 
+
+    //  🧠 use Effect : Run Once when the component mount
+    useEffect(()=>{
+        const timer = setTimeout(()=>{
+            setIsRead(true);
+        },2000);// 2 sec
+        // Optional Cleanup (in case of component is removed)
+        return ()=>clearTimeout(timer);
+    },[]);// empty dependency array = run only once on mount
     return(
         <div style={{
             border: "1px solid #ccc",
@@ -15,6 +26,7 @@ function MessageCard({sender, text}){
         }}>
             <h3>✉️ From {sender}</h3>
             <p>{text}</p>
+            <p>Status: {isRead ? "✅Read" : "⌛Unread"}</p>
             <button onClick={handleLikes}>🩷 Like</button>
             <p>Likes:{like}</p>
         </div>
